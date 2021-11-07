809f0334da307bb08bff6f61b375d37413a29c65
ljldgup <1142509832@qq.com>
2021-11-07 19:28:24 +0800

    注入流程测试

--- SpringBoot/NoWeb/src/main/java/ljl/Application.java
+++ SpringBoot/NoWeb/src/main/java/ljl/Application.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.annotation.Resource;
 
 import ljl.DemoService;
+import ljl.service.FactoriesTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -12,16 +13,25 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication  
 public class Application implements ApplicationRunner{  
-    //@Resource和@Autowired都是做bean的注入时使用
-    //@Resource按名称导入
+
 	@Resource
     private DemoService demoService;
 
 	@Autowired
     private ConditionTest conditionTest;
+
+	//@Resource和@Autowired都是做bean的注入时使用
+	//@Resource按名称导入
+	@Resource(name = "factoriesTestImpl2")
+    private FactoriesTest factoriesTest;
+
+	@Autowired
+	ApplicationContext applicationContext;
+
 	
     public static void main(String[] args) {  
         new SpringApplicationBuilder(Application.class)  
@@ -31,7 +41,9 @@ public class Application implements ApplicationRunner{
   
     @Override  
     public void run(ApplicationArguments args) throws Exception {  
-        while(true) {  
+        while(true) {
+			System.out.println(applicationContext.getParentBeanFactory());
+            factoriesTest.print();
         	demoService.test();
             System.out.println("now is " + new Date().toLocaleString());  
             Thread.sleep(1000);  
