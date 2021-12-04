c10f3a029ffed6233d3bce33a451d8d3b1cbec61
ljldgup <1142509832@qq.com>
2021-12-05 01:53:32 +0800

    替换默认线程池，拒绝策略测试

--- SpringBoot/aopRelated/complete/src/main/java/Application.java
+++ SpringBoot/aopRelated/complete/src/main/java/Application.java
@@ -1,6 +1,7 @@
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
@@ -9,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.*;
 
 @SpringBootApplication
 //导入xml配置
@@ -26,7 +29,14 @@ public class Application {
 	String cheers(@RequestParam(name="name", required=false, defaultValue="defaultValue") String name) {
 		return String.format("xml aop test name=%s!", name);
 	}
-	
+
+	//如果线程池的拒绝策略设置成DiscardPolicy或者DiscardOldestPolicy，通过Future获取执行结果，可能导致线程会一直阻塞。
+
+	@Bean
+	public ThreadPoolExecutor customExecutor(){
+		return new ThreadPoolExecutor(1, 2, 1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5),
+				Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+	}
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- SpringBoot/aopRelated/complete/src/main/java/bean/AsyncTest.java
+++ SpringBoot/aopRelated/complete/src/main/java/bean/AsyncTest.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component("debugAsyncTest")
@@ -22,7 +23,7 @@ public class AsyncTest {
 	@Async
 	public void asyncTest1() throws InterruptedException {
 		TimeUnit.SECONDS.sleep(10);
-		System.out.println("async with no return "+ atomicInteger.getAndAdd(1));
+		System.out.println("async with no return " + atomicInteger.getAndAdd(1));
 	}
 
 	//直接报错
@@ -34,12 +35,20 @@ public class AsyncTest {
 
 	@Scheduled(cron = "*/1 * * * * *")
 	public void test() throws InterruptedException, ExecutionException {
+		//抛出异常后仍然能够继续执行,线程池会自动创建新线程
+		if (asyncTest2().get() % 2 == 0) throw new RuntimeException();
+
 		//被代理的类内调用
 		AsyncTest asyncTest = (AsyncTest) applicationContext.getBean(this.getClass());
 		System.out.println("call asyncTest1");
 		asyncTest.asyncTest1();
 		System.out.println("call asyncTest2");
 		Future<Integer> asyncTest2 = asyncTest.asyncTest2();
-		System.out.println("async with return " + asyncTest2.get());
+		try {
+			//如果线程池使用Discardpolicy 拒绝策略，使用带超时时间的get方法
+			System.out.println("async with return " + asyncTest2.get(1,TimeUnit.SECONDS));
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		}
 	}
 }
